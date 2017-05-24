96b3ac3b27ecc1a52539c1bc7be8765e1744ebf1
Dileep Gadiraju <dileepgadiraju@Dileeps-MacBook-Pro.local>
2017-05-23 20:41:26 -0400

    Completed table view ui design base structure

--- devslopes-social/Constants.swift
+++ devslopes-social/Constants.swift
@@ -6,7 +6,8 @@
 //  Copyright © 2017 Dileep Gadiraju. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 
 let KEY_UID = "uid"
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
--- devslopes-social/FancyBtn.swift
+++ devslopes-social/FancyBtn.swift
@@ -1,22 +1,23 @@
 //
-//  FancyView.swift
+//  FancyBtn.swift
 //  devslopes-social
 //
-//  Created by Dileep Gadiraju on 5/17/17.
+//  Created by Dileep Gadiraju on 5/22/17.
 //  Copyright © 2017 Dileep Gadiraju. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class FancyView: UIView {
-
+class FancyBtn: UIButton {
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-      //layer.shadowColor = CGColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6)
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
         layer.cornerRadius = 2.0
     }
-
+    
 }
